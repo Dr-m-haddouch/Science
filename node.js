c92918b2d4018dd6bb2befaffef47a37fb9d4f66
Drhaddouch <157210455+Dr-m-haddouch@users.noreply.github.com>
2024-02-03 22:10:27 +0100

    node.js

--- node.js
+++ node.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all routes
 
 app.post('/authenticate', (req, res) => {
     const { username, password } = req.body;
